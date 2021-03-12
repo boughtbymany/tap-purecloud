6671bfdf1de65c19e5a637e27c8625d0256c7eb3
Jean Rumeau <rumeau@gmail.com>
2021-03-12 01:06:29 -0300

    Fix dependencies, again

--- setup.py
+++ setup.py
@@ -12,8 +12,11 @@ setup(name='tap-purecloud',
       py_modules=['tap_purecloud'],
       install_requires=[
           'singer-python==5.12.0',
+          'backoff==1.10.0',
+          'requests==2.25.1',
           'python-dateutil==2.8.1',
-          'PureCloudPlatformClientV2==111.0.0'
+          'PureCloudPlatformClientV2==111.0.0',
+          'websockets==8.1'
       ],
       entry_points='''
           [console_scripts]
